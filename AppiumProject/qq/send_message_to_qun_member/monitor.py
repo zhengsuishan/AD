105b8e3b0396557f93944c5887ef43c0c1a11e5b
zhengsuishan <875707618@qq.com>
2019-01-09 18:03:39 +0800

    Merge branch 'master' of https://github.com/zhengsuishan/AD
    # Conflicts:
    #	AppiumProject/demo.py

--- AppiumProject/qq/send_message_to_qun_member/monitor.py
+++ AppiumProject/qq/send_message_to_qun_member/monitor.py
@@ -4,6 +4,12 @@ import time
 file_name = 'data.xlsx' #文件路径
 first = True
 
+udid = 'd102deb37d13'
+
+# 设置输入法
+setting_cmd = 'adb -s %s shell ime set io.appium.android.ime/.UnicodeIME' % udid
+os.popen(setting_cmd)
+
 while True:
     if first:
         py_path = 'qun_message.py'
