dcfe0bc07f1ae94a9bfa6109b535fbef5236c9f9
zhengsuishan <875707618@qq.com>
2019-01-14 18:05:56 +0800

    Merge branch 'master' of https://github.com/zhengsuishan/AD
    # Conflicts:
    #	AppiumProject/demo.py

--- AppiumProject/qq/send_message_to_qun_member/new_list.py
+++ AppiumProject/qq/send_message_to_qun_member/new_list.py
@@ -3,14 +3,16 @@
 import random
 
 news_url_list = ['【会赚钱的头条】只要填写我的邀请码81201961，你立赚1元现金，我立赚2元现金，↓↓快来点击下载帮我助力吧↓↓http://a2.app.qq.com/o/simple.jsp?pkgname=com.cashtoutiao&ckey=CK1371494628908',
-                 '东方头条【【热】识破微信诈骗五大伎俩，别-入-坑-】https://mini.eastday.com/mobile/190113082723493.html?ca=809169121&f1=xq1',
-                 '两个人在一起是不是真合适，就看做2件事是否很融洽。详情链接>>>http://www.xunqdq.cn/iABvUfwpy43xk',
-                 '离婚考卷红了！分数太高的夫妻不准离婚！你能做对几道？。详情链接>>>http://www.weixinhehua.cn/rayiAzzd4yzjxb']
+                 '人为什么离婚，这是我见过最理性的回答。详情链接>>>http://www.oulemyall.cn/2Iveiyycpefhnb',
+                 '老同学是什么?看完我真的哭了！送给我最珍惜的老同学们~。详情链接>>>http://www.weixinfangsheng.cn/6RnaMz243g2n9',
+                 '东方头条【【热文】2019爱自己，做自己，靠-自-己-】https://mini.eastday.com/mobile/190114000853918.html?ca=809169121&f1=xq1',
+                 '东方头条【【热】不懂英语怎么办？4个华为手机自带语言翻译功能，让英语-畅-通-无-阻-】https://mini.eastday.com/mobile/190113204953244.html?ca=809169121&f1=xq1']
 
 news_text_list = ['看新闻视频赚钱，1元即可提现，娱乐赚钱两不误。',
-                  '识破微信诈骗五大伎俩，别-入-坑',
-                  '两个人在一起是不是真合适，就看做2件事是否很融洽',
-                  '离婚考卷红了！分数太高的夫妻不准离婚！你能做对几道']
+                  '人为什么离婚，这是我见过最理性的回答',
+                  '老同学是什么?看完我真的哭了！送给我最珍惜的老同学们',
+                  '2019爱自己，做自己，靠-自-己',
+                  '不懂英语怎么办？4个华为手机自带语言翻译功能，让英语-畅-通-无-阻']
 
 def get_news():
     size = len(news_url_list)
