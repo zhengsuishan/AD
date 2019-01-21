9236b63ca2131ca383dd191c237a1c92f320e8fe
zss <875707618@qq.com>
2019-01-21 08:29:21 +0800

    new

--- AppiumProject/qq/send_message_to_qun/init_para.py
+++ AppiumProject/qq/send_message_to_qun/init_para.py
@@ -3,8 +3,8 @@
 class InitParm(object):
 
     device_udid = 'd102deb37d13'
-    send_text = '妻子是处女，新婚夜一碰她就害怕，趁她洗澡我偷偷看摄像头吓坏了。详情链接>>>http://www.https-letou.cn/mI7Z3ezfypj2k'
-    send_text1 = '妻子是处女，新婚夜一碰她就害怕，趁她洗澡我偷偷看摄像头吓坏了'
+    send_text = '中国工资最低的6个城市，其中有你的故乡吗？。详情链接>>>http://www.weixinbenshan.cn/yYzy2uu4yp0du'
+    send_text1 = '中国工资最低的6个城市，其中有你的故乡吗'
     qun_name_list1 = ['正定网-买卖交易', 'ATX(AutomatorX) old', '北京化妆师交流群', '珠三角高级化妆师群', '重庆软件测试交流群', '南京软件测试交流群',
                      '长沙软件测试交流群'] #群名称
     qun_name_list0 = ['兄弟汇', '路遇行人', 'Microsoft问题交流', '190933303', '微博热门实时推广精选', '输了你赢了全世界又如',
