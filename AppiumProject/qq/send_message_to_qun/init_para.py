9e8c4cc02782e6817f41b2581500d11494527d73
zhengsuishan <875707618@qq.com>
2019-01-21 18:02:36 +0800

    Merge branch 'master' of https://github.com/zhengsuishan/AD
    # Conflicts:
    #	AppiumProject/demo.py

--- AppiumProject/qq/send_message_to_qun/init_para.py
+++ AppiumProject/qq/send_message_to_qun/init_para.py
@@ -3,8 +3,8 @@
 class InitParm(object):
 
     device_udid = 'd102deb37d13'
-    send_text = '中国工资最低的6个城市，其中有你的故乡吗？。详情链接>>>http://www.weixinbenshan.cn/yYzy2uu4yp0du'
-    send_text1 = '中国工资最低的6个城市，其中有你的故乡吗'
+    send_text = '2019十大最佳旅游城市，中国仅一座城市上榜，但高居“第二”。详情链接>>>http://www.sshfcmy.cn/26nEVjhz34p2u'
+    send_text1 = '2019十大最佳旅游城市，中国仅一座城市上榜，但高居“第二”'
     qun_name_list1 = ['正定网-买卖交易', 'ATX(AutomatorX) old', '北京化妆师交流群', '珠三角高级化妆师群', '重庆软件测试交流群', '南京软件测试交流群',
                      '长沙软件测试交流群'] #群名称
     qun_name_list0 = ['兄弟汇', '路遇行人', 'Microsoft问题交流', '190933303', '微博热门实时推广精选', '输了你赢了全世界又如',
--- AppiumProject/qq/send_message_to_qun_member/new_list.py
+++ AppiumProject/qq/send_message_to_qun_member/new_list.py
@@ -2,13 +2,9 @@
 
 import random
 
-news_url_list = ['【会赚钱的头条】只要填写我的邀请码81201961，你立赚1元现金，我立赚2元现金，↓↓快来点击下载帮我助力吧↓↓http://a2.app.qq.com/o/simple.jsp?pkgname=com.cashtoutiao&ckey=CK1371494628908',
-                 '早晨洗头发和晚上洗头发，哪个危害更大？多长时间洗一次最健康？。详情链接>>>http://www.taoyuan1688y.cn/MVnMZzq434uxu',
-                 '他首次剖白和周星驰的真实关系，揭露17年从没合-作-的-原-因-】https://mini.eastday.com/mobile/190115071300170.html?ca=809169121&f1=xq1']
+news_url_list = ['手机赚钱项目交流群，群聊号码：849597564，有兴趣的欢迎加群']
 
-news_text_list = ['看新闻视频赚钱，1元即可提现，娱乐赚钱两不误。',
-                  '早晨洗头发和晚上洗头发，哪个危害更大？多长时间洗一次最健康',
-                  '他首次剖白和周星驰的真实关系，揭露17年从没合-作-的-原-因']
+news_text_list = ['入群答案：手机赚钱']
 
 def get_news():
     size = len(news_url_list)
